e21237a4670304c72f61b01e5245894135720cc9
Ted Kiriakidi <fedorkiriakidi@gmail.com>
2021-03-05 16:52:35 -0500

    Modified configs for deployment.

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -35,7 +35,7 @@ config :event_manager, EventManagerWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :event_manager, EventManagerWeb.Endpoint, server: true
+config :event_manager, EventManagerWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
